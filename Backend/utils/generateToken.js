50a933fbbde44a2a10ae3ea9311fd4f37dd97ad9
basitol <basitolaitan27@gmail.com>
2022-11-13 19:57:04 +0100

    build: token generator function

---
+++ Backend/utils/generateToken.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      verifyToken: user.verifyToken,
+      verified: user.verified,
+    },
+    process.env.JWT_SECRET || "somethingsecret",
+    {
+      expiresIn: "30d",
+    }
+  );
+};
+
+module.exports = generateToken;
