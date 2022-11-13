9acb3aff88fc99028abbc185822f1e3ad88054bb
basitol <basitolaitan27@gmail.com>
2022-11-13 19:54:39 +0100

    build: auth middleware

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res
+      .status(401)
+      .json({ message: "Not authorized to access this route" });
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Not authorized to access this route" });
+  } else {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      return res
+        .status(401)
+        .json({ message: "Not authorized to access this route" });
+    }
+  }
+};
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    return res.status(401).json({ message: "Not authorized as an admin" });
+  }
+};
+
+module.exports = { protect, admin };
