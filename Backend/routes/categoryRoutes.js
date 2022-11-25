b60d89525813cae7c54a5bb8f78f8fe13bd20d76
basitol <basitolaitan27@gmail.com>
2022-11-25 23:12:39 +0100

    update: category routes

--- Backend/routes/categoryRoutes.js
+++ Backend/routes/categoryRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const router = express.Router;
-const createCategory  = require("../controllers/categoryController");
+const router = express.Router();
+const {
+  createCategory,
+  getCategories,
+  getMealsByCategory,
+} = require("../controllers/categoryController");
+const { protect, verify, admin } = require("../middleware/authMiddleware");
 
-router.route("/").post(createCategory);
+router.route("/").post(protect, verify, admin, createCategory);
+router.route("/").get(getCategories);
+router.route("/:category").get(getMealsByCategory);
 
 module.exports = router;
