4f6bcd02f385e39e1ab4f3793e97a06f019ef12f
basitol <basitolaitan27@gmail.com>
2022-12-09 02:06:08 +0100

    update: index and got ignore

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 require("./startup/routes")(app);
 require("./startup/db")();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
